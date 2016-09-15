b438d176a32de87e8dfdb9553b9c0447bcc017d6
Jacek Wieczorek <mrjjot@gmail.com>
2016-09-15 20:10:58 +0200

    Fix that thing.

--- include/onewire.h
+++ include/onewire.h
@@ -14,7 +14,7 @@
 #define ONEWIRE_ERROR_OK 	0
 #define ONEWIRE_ERROR_COMM 	1
 
-extern unsigned char onewireInit( uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask );
+extern unsigned char onewireInit( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask );
 extern void onewireWrite( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask, uint8_t data );
 extern unsigned char onewireRead( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask );
 
