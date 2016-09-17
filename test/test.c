925e4fc238a0db08c2dd2f7b6e36589fd919b54d
Jacajack <Jacajack@wp.pl>
2016-09-17 17:38:44 +0200

    Change test code a little bit

--- test/test.c
+++ test/test.c
@@ -9,17 +9,21 @@ char txt[80];
 
 int main( )
 {
+	int temp;
 	uartInit( 9600 );
 
 	while ( 1 )
 	{
-		//Read temperature
+		//Start conversion
 		ds18b20request( &PORTB, &DDRB, &PINB, ( 1 << 0 ) );
-		sprintf( txt, "raw: %d\n", ds18b20read( &PORTB, &DDRB, &PINB, ( 1 << 0 ) ) );
-		uartSend( txt );
 
 		//Delay
 		_delay_ms( 1000 );
+
+		//Read temperature
+		ds18b20read( &PORTB, &DDRB, &PINB, ( 1 << 0 ), &temp );
+		sprintf( txt, "raw: %d\n", temp );
+		uartSend( txt );	
 	}
 
 	return 0;
