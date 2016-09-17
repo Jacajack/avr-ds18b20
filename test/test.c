6bc7236622d0165f02a4c73761c971da451f2f6e
Jacajack <Jacajack@wp.pl>
2016-09-18 00:40:57 +0200

    Update test code

--- test/test.c
+++ test/test.c
@@ -15,15 +15,15 @@ int main( )
 	while ( 1 )
 	{
 		//Start conversion
-		ds18b20request( &PORTB, &DDRB, &PINB, ( 1 << 0 ) );
+		ds18b20request( &PORTB, &DDRB, &PINB, ( 1 << 0 ), NULL );
 
 		//Delay
 		_delay_ms( 1000 );
 
 		//Read temperature
-		ds18b20read( &PORTB, &DDRB, &PINB, ( 1 << 0 ), &temp );
+		ds18b20read( &PORTB, &DDRB, &PINB, ( 1 << 0 ), NULL, &temp );
 		sprintf( txt, "raw: %d\n", temp );
-		uartSend( txt );	
+		uartSend( txt );
 	}
 
 	return 0;
