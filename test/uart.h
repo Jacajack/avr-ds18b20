9ff45d364f437277585a191c1af29a02a9181f64
Jacek Wieczorek <mrjjot@gmail.com>
2016-09-22 21:45:46 +0200

    Remove test directory

--- test/uart.h
+++
@@ -1,7 +0,0 @@
-extern void uartInit( unsigned int );
-extern void uartSendChar( char );
-extern void uartSend( const char * );
-extern char uartReadChar( );
-extern void uartRead( );
-
-extern char UARTbuffer[80];
