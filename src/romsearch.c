cefeee3e09a46f5eeaa12464f9288121f5a88f31
Jacek Wieczorek <mrjjot@gmail.com>
2018-09-29 14:15:24 +0200

    Fix possible strict aliasting and make conform to -Wpedantic

--- src/romsearch.c
+++ src/romsearch.c
@@ -9,6 +9,7 @@
 /**
 	\file
 	\brief Implements functions for searching for connected sensors
+	\todo Optimizations and improvements
 */
 
 
@@ -65,9 +66,10 @@ static inline uint8_t ckolder( uint8_t *arr, uint8_t len, uint16_t n )
 uint8_t ds18b20search( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask, uint8_t *romcnt, uint8_t *roms, uint16_t buflen )
 {
 	uint8_t i, bit, currom = 0;
-	uint8_t junction[8] = {0};
+	uint64_t j;
+	uint64_t *jun =  &j;
 	uint8_t sreg = SREG;
-	uint64_t *jun = (uint64_t*) junction; //Not to break strict aliasing rules (or at least skip the warning)
+	uint8_t *junction = (uint8_t*) &j;
 
 	if ( romcnt == NULL ) return DS18B20_ERROR_OTHER;
 
@@ -96,20 +98,20 @@ uint8_t ds18b20search( volatile uint8_t *port, volatile uint8_t *direction, vola
 			switch ( bit )
 			{
 				//Received 11 - no sensors connected
-				case 0b11:
+				case 3:
 					*romcnt = 0; //Null pointer check is at the begining
 					SREG = sreg;
 					return DS18B20_ERROR_COMM;
 					break;
 
 				//Received 10 or 01 - ROM bits match
-				case 0b10:
-				case 0b01:
+				case 1:
+				case 2:
 					bit = bit & 1;
 					break;
 
 				//Received 00 - ROM bits differ
-				case 0b00:
+				case 0:
 					//Check if there are older junction bits set
 					if ( ckolder( junction, 8, i ) )
 					{
